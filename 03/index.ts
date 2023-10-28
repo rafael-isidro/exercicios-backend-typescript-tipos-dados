8413c1ae1f0a4974bebca74207a02d63bfc42ab5
rafael-isidro <rafa-isidro@hotmail.com>
2023-10-28 15:00:09 -0300

    Exercício 03 - Módulo 02 - Finalizado

--- 03/index.ts
+++ 03/index.ts
@@ -1,11 +1,14 @@
-const mountMultiplicationTable = (numberList: number[]): void => {
-  for (let number of numberList) {
-    for (let i = 0; i <= 10; i++) {
-      const result: number = number * i;
-      console.log(`${number} x ${i} = ${result}`);
+const mountMultiplicationTable = (numberList: number[]): string => {
+    let result: string = "";
+    for (let number of numberList) {
+      for (let i = 0; i <= 10; i++) {
+        const multiplication: number = number * i;
+        result += `${number} x ${i} = ${multiplication} \n`;
+        if (i === 10) result += "--------------- \n";
+      }
     }
-    console.log("---------------");
-  }
-};
-
-mountMultiplicationTable([3, 2, 9, 0]);
+    return result;
+  };
+  
+  console.log(mountMultiplicationTable([3, 2, 9, 0]));
+  
