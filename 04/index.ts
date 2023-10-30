49765218ac57fe7367f4b4f448a3eb662d8b4481
rafael-isidro <rafa-isidro@hotmail.com>
2023-10-30 10:11:45 -0300

    Exercício 03 - Módulo 02 - Finalizado

---
+++ 04/index.ts
@@ -0,0 +1,27 @@
+const createProductLabel = (product: {
+  produto: string;
+  lote: number;
+  ano: number;
+  qtd: number;
+}): string[] => {
+  const labelArray: string[] = [];
+
+  for (let i = 1; i <= product.qtd; i++) {
+    const labelLote = product.lote.toString().padStart(3, "0");
+    const labelItem = i.toString().padStart(3, "0");
+    
+    const productLabel = `${labelLote}-${product.ano}-${labelItem}`;
+    labelArray.push(productLabel);
+  }
+
+  return labelArray;
+};
+
+console.log(
+  createProductLabel({
+    produto: "Teclado Mecânico",
+    lote: 105,
+    ano: 2022,
+    qtd: 10,
+  })
+);
